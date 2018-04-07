2cb895277185b8345d43bfca50628d05c7d0c5c0
redwanh25 <32978751+redwanh25@users.noreply.github.com>
2018-04-08 01:11:55 +0600

    update

--- app/src/main/java/com/example/redwan/firebasechatapp/FullScreenImageView.java
+++ app/src/main/java/com/example/redwan/firebasechatapp/FullScreenImageView.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 public class FullScreenImageView extends AppCompatActivity {
@@ -13,12 +15,22 @@ public class FullScreenImageView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_image_view);
-        ImageView imageView = findViewById(R.id.image_fullScreen);
+        final ImageView imageView = findViewById(R.id.image_fullScreen);
 
         Intent intent = getIntent();
-        String call = intent.getType();
+        final String image = intent.getType();
 
-        Picasso.with(this).load(call).placeholder(R.drawable.avatar_default).into(imageView);
+        Picasso.with(this).load(image).networkPolicy(NetworkPolicy.OFFLINE).placeholder(R.drawable.avatar_default).into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onError() {
+                Picasso.with(getApplicationContext()).load(image).placeholder(R.drawable.avatar_default).into(imageView);
+            }
+        });
     }
 
 
